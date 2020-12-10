649eb5f3110b8d045d585f216d282eba5c51d2f3
Daniel Mendler <mail@daniel-mendler.de>
2020-12-10 19:44:35 +0100

    update description

--- consult.el
+++ consult.el
@@ -26,13 +26,19 @@
 ;;; Commentary:
 
 ;; Consult implements a set of commands which use `completing-read' to select
-;; from a list of candidates. Most provided commands follow the naming scheme `consult-thing`.
-;; Some commands are drop-in replacements for existing functions, e.g., consult-apropos.
-;; Other commands provide additional non-existing functionality, e.g., consult-line.
+;; from a list of candidates. Most provided commands follow the naming scheme
+;; `consult-<thing>'. Some commands are drop-in replacements for existing
+;; functions, e.g., `consult-apropos' or the enhanced buffer switcher
+;; `consult-buffer.' Other commands provide additional functionality, e.g.,
+;; `consult-line', to search for a line. Many commands support candidate
+;; preview. If a candidate is selected in the completion view, the buffer shows
+;; the candidate immediately.
 
-;;; This package is inspired by and partially derived from counsel by Oleh Krehel,
-;;; Copyright Free Software Foundation, Inc.
-;;; Furthermore some of the commands found in this package were taken from the Selectrum wiki.
+;;; Acknowledgements:
+
+;; This package took inspiration from Counsel by Oleh Krehel. Some of the
+;; commands found in this package originated in the Selectrum wiki. See the
+;; README for a full list of contributors.
 
 ;;; Code:
 
