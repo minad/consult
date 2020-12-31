de3cb45fe52a8ca72b6949d51ba2f2fbbc34de69
Daniel Mendler <mail@daniel-mendler.de>
2020-12-31 09:54:08 +0100

    fix #107

--- consult.el
+++ consult.el
@@ -1229,7 +1229,7 @@ The alist contains (string . position) pairs."
   (consult--jump
    (consult--read "Go to global mark: "
                   (consult--with-increased-gc (consult--global-mark-candidates))
-                  :category 'xref-location
+                  :category 'line
                   :sort nil
                   :require-match t
                   :lookup #'consult--lookup-cdr
