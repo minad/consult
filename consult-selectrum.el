babc18fd4ec9648ba94cb8d4a7fb851dd166d346
Daniel Mendler <mail@daniel-mendler.de>
2021-01-25 18:01:51 +0100

    Fix async filtering issue reported by @clemera
    The problem is that async-split-setup modified
    both the refine function AND the completion style.
    This would result in a duplicate operation if Selectrum
    uses the completion style itself for filtering.
    
    It should work now thanks to
    
    https://github.com/raxod502/selectrum/commit/257224bc7c5e97a52cc56cef2930f548af3a83e9

--- consult-selectrum.el
+++ consult-selectrum.el
@@ -57,7 +57,7 @@
   "Refresh selectrum view."
   (and selectrum-active-p (selectrum-exhibit 'keep-selected)))
 
-(cl-defun consult-selectrum--setup (_prompt candidates &key default-top &allow-other-keys)
+(cl-defun consult-selectrum--read-setup-adv (_prompt candidates &key default-top &allow-other-keys)
   "Advice for `consult--read-setup' for Selectrum specific setup.
 
 See `consult--read' for the CANDIDATES and DEFAULT-TOP arguments."
@@ -70,18 +70,22 @@ See `consult--read' for the CANDIDATES and DEFAULT-TOP arguments."
   (lambda (str cands)
     (funcall orig (substring str (cdr (consult--async-split-string str))) cands)))
 
-(defun consult-selectrum--async-split-setup ()
-  "Advice for `consult--async-split-setup' to be used by Selectrum."
-  (setq-local selectrum-refine-candidates-function
-              (consult-selectrum--async-split-wrap selectrum-refine-candidates-function))
-  (setq-local selectrum-highlight-candidates-function
-              (consult-selectrum--async-split-wrap selectrum-highlight-candidates-function)))
+(defun consult-selectrum--async-split-setup-adv (fun)
+  "Advice for `consult--async-split-setup' to be used by Selectrum.
+
+FUN is the original function."
+  (if (not selectrum-active-p)
+      (funcall fun)
+    (setq-local selectrum-refine-candidates-function
+		(consult-selectrum--async-split-wrap selectrum-refine-candidates-function))
+    (setq-local selectrum-highlight-candidates-function
+		(consult-selectrum--async-split-wrap selectrum-highlight-candidates-function))))
 
 (add-hook 'consult--completion-filter-hook #'consult-selectrum--filter)
 (add-hook 'consult--completion-candidate-hook #'consult-selectrum--candidate)
 (add-hook 'consult--completion-refresh-hook #'consult-selectrum--refresh)
-(advice-add #'consult--read-setup :before #'consult-selectrum--setup)
-(advice-add #'consult--async-split-setup :before #'consult-selectrum--async-split-setup)
+(advice-add #'consult--read-setup :before #'consult-selectrum--read-setup-adv)
+(advice-add #'consult--async-split-setup :around #'consult-selectrum--async-split-setup-adv)
 
 (provide 'consult-selectrum)
 ;;; consult-selectrum.el ends here
