3bb31bd5f23a2c5fbc3b9d314e60f85b6f9e11c2
Daniel Mendler <mail@daniel-mendler.de>
2021-02-12 23:04:42 +0100

    consult--default-completion-candidate: Return minibuffer-default
    This aligns the preview behavior of icomplete/default completion
    with Selectrum. @oantolin, is this relevant for Embark too?
    Should Embark do the same when retrieving the completion target?

--- consult.el
+++ consult.el
@@ -3698,14 +3698,20 @@ See `consult-grep' for more details regarding the asynchronous search."
   "Return current candidate from default completion system or icomplete."
   (when (eq completing-read-function #'completing-read-default)
     (let ((cand (minibuffer-contents-no-properties)))
-      ;; When the current minibuffer content matches a candidate, return it!
-      (if (test-completion cand
-                           minibuffer-completion-table
-                           minibuffer-completion-predicate)
-          cand
-        ;; Otherwise return the first candidate of the sorted completion list.
-        (when completion-all-sorted-completions
-          (car completion-all-sorted-completions))))))
+      (cond
+       ;; When the current minibuffer content matches a candidate, return it!
+       ((test-completion cand
+                         minibuffer-completion-table
+                         minibuffer-completion-predicate)
+        cand)
+       ;; Return the first candidate of the sorted completion list.
+       (completion-all-sorted-completions
+        (car completion-all-sorted-completions))
+       ;; Return the default candidate
+       ((and minibuffer-default (string= cand ""))
+        (if (consp minibuffer-default)
+            (car minibuffer-default)
+          minibuffer-default))))))
 
 (defun consult--default-completion-filter (category _highlight)
   "Return default filter function given the completion CATEGORY.
