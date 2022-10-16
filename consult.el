cf9c5e5d0c060189fec174e65d8deb8c77cd1580
Daniel Mendler <mail@daniel-mendler.de>
2022-10-16 15:43:05 +0200

    Update commentary

--- consult.el
+++ consult.el
@@ -26,22 +26,26 @@
 
 ;;; Commentary:
 
-;; Consult implements a set of `consult-<thing>' commands which use
-;; `completing-read' to select from a list of candidates. Consult
-;; provides an enhanced buffer switcher `consult-buffer' and search and
-;; navigation commands like `consult-imenu' and `consult-line'.
-;; Searching through multiple files is supported by the asynchronous
-;; `consult-grep' command. Many Consult commands allow previewing
-;; candidates - if a candidate is selected in the completion view, the
-;; buffer shows the candidate immediately.
+;; Consult implements a set of `consult-<thing>' commands, which aim to
+;; improve the way you use Emacs. The commands are founded on
+;; `completing-read', which selects from a list of candidate strings.
+;; Consult provides an enhanced buffer switcher `consult-buffer' and
+;; search and navigation commands like `consult-imenu' and
+;; `consult-line'. Searching through multiple files is supported by the
+;; asynchronous `consult-grep' command. Many Consult commands support
+;; previewing candidates. If a candidate is selected in the completion
+;; view, the buffer shows the candidate immediately.
 
-;; The Consult commands are compatible with completion systems based on
-;; the Emacs `completing-read' API, including the default completion
-;; system, Vertico, Mct and Icomplete.
+;; The Consult commands are compatible with multiple completion systems
+;; based on the Emacs `completing-read' API, including the default
+;; completion system, Vertico, Mct and Icomplete.
 
-;; Consult has been inspired by Counsel. Some of the Consult commands
-;; originated in the Counsel package or the wiki of the Selectrum
-;; package. See the README for a full list of contributors.
+;; See the README for an overview of the available Consult commands and
+;; the documentation of the configuration and installation of the
+;; package.
+
+;; The full list of contributors can be found in the acknowledgments
+;; section of the README.
 
 ;;; Code:
 
