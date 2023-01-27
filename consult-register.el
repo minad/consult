92458c95f471602eb4f050ba3e3030e22ba7533d
Daniel Mendler <mail@daniel-mendler.de>
2023-01-27 23:33:01 +0100

    Add missing require

--- consult-register.el
+++ consult-register.el
@@ -24,6 +24,7 @@
 ;;; Code:
 
 (require 'consult)
+(require 'kmacro)
 
 (defcustom consult-register-prefix #("#" 0 1 (face consult-key))
   "Prepend prefix in front of register keys during completion."
