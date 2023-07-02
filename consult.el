fe49dedd71802ff97be7b89f1ec4bd61b98c2b13
Daniel Mendler <mail@daniel-mendler.de>
2023-07-02 10:19:13 +0200

    Version 0.35

--- consult.el
+++ consult.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler and Consult contributors
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2020
-;; Version: 0.34
+;; Version: 0.35
 ;; Package-Requires: ((emacs "27.1") (compat "29.1.4.1"))
 ;; Homepage: https://github.com/minad/consult
 ;; Keywords: matching, files, completion
