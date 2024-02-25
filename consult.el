f21135efcff4503e4557566c492f403cfb921c54
Daniel Mendler <mail@daniel-mendler.de>
2024-02-25 17:10:44 +0100

    consult-preview-at-point-mode: Remove superfluous :init-value keyword argument

--- consult.el
+++ consult.el
@@ -5108,7 +5108,7 @@ the asynchronous search."
   "Preview minor mode for *Completions* buffers.
 When moving around in the *Completions* buffer, the candidate at point is
 automatically previewed."
-  :init-value nil :group 'consult
+  :group 'consult
   (if consult-preview-at-point-mode
       (add-hook 'post-command-hook #'consult-preview-at-point nil 'local)
     (remove-hook 'post-command-hook #'consult-preview-at-point 'local)))
