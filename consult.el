0c3f53916ea0db0c472c0a0c620a85cc1b00caf2
Daniel Mendler <mail@daniel-mendler.de>
2024-08-24 10:00:54 +0200

    Revert 3d0fc6a8c6a74b21ca854b8632b3f58e0e513b85

--- consult.el
+++ consult.el
@@ -817,10 +817,9 @@ asked for the directories or files to search via
                               (lambda ()
                                 (setq-local completion-ignore-case ignore-case)
                                 (set-syntax-table minibuffer-local-filename-syntax))
-                            (mapcar #'substitute-in-file-name
-                                    (completing-read-multiple "Directories or files: "
-                                                              #'completion-file-name-table
-                                                              nil t def 'consult--path-history def)))))
+                            (completing-read-multiple "Directories or files: "
+                                                      #'completion-file-name-table
+                                                      nil t def 'consult--path-history def))))
                  ((and `(,p) (guard (file-directory-p p))) p)
                  (ps (setq paths (mapcar (lambda (p)
                                            (file-relative-name (expand-file-name p)))
